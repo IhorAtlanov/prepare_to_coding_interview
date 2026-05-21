35a38aaa87a20356fb91730c8539c15b12edf225
IhorAtlanov <iatlanov04@gmail.com>
2026-05-21 17:44:53 +0200

    278. First Bad Version

--- Easy/67_Add_Binary.py
+++ Easy/67_Add_Binary.py
@@ -16,7 +16,7 @@ Constraints:
 a and b consist only of '0' or '1' characters.
 Each string does not contain leading zeros except for the zero itself.
 """
-
+"Solution: https://www.youtube.com/watch?v=keuWJ47xG8g"
 a = "11"
 b = "1"
 
